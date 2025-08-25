5f55c3a8a8437b31c53ec69a29075d806d5778f7
Zachary Boehm <zboehm@menard-inc.com>
2025-08-25 16:07:42 -0500

    [feat] start generator by adding nuget package and iterating the winmd files

---
+++ MetadataGenerator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+internal class Program
+{
+    static IEnumerable<string> FindWinmds(string root)
+    {
+        if (!Directory.Exists(root))
+            yield break;
+
+        foreach (var path in Directory.EnumerateFiles(root, "*.winmd", SearchOption.AllDirectories))
+        {
+            yield return path;
+        }
+    }
+
+    private static int Main()
+    {
+        string dir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location!)!, "winmd"); 
+        List<string> paths = new List<string>();
+
+        foreach (var winmd in FindWinmds(dir))
+        {
+            paths.Add(winmd);
+        }
+
+        paths.Sort();
+        foreach (var path in paths)
+        {
+            Console.WriteLine("{0}", Path.GetFileNameWithoutExtension(path));
+        }
+
+        return 0;
+    }
+}
