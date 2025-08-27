374068adbf3ece5dda3119761029052d48760737
Tired-Fox <zboehm104@gmail.com>
2025-08-26 22:47:56 -0500

    [feat] organize code into function that handles downloading metadata files

--- generator/src/main.zig
+++ generator/src/main.zig
@@ -43,60 +43,146 @@ const Asset = struct {
 const Config = struct {
     current: []const u8 = "",
     digest: []const u8 = "",
-};
 
-pub fn main() !void {
-    var gpa = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer gpa.deinit();
-    const allocator = gpa.allocator();
-
-    var config = blk: {
+    pub fn parse(allocator: std.mem.Allocator, path: []const u8) !@This() {
         const config_slice = std.fs.cwd().readFileAllocOptions(
             allocator,
-            "config.zon",
+            path,
             std.math.maxInt(usize),
             null,
             1,
             0
         ) catch |err| switch (err) {
-            error.FileNotFound => break :blk Config { },
+            error.FileNotFound => return .{ },
             else => return err,
         };
         defer allocator.free(config_slice);
 
         if (config_slice.len == 0) {
-            break :blk Config { };
+            return .{ };
         }
 
-        break :blk try std.zon.parse.fromSlice(Config, allocator, config_slice[0..], null, .{
+        return try std.zon.parse.fromSlice(Config, allocator, config_slice[0..], null, .{
             .ignore_unknown_fields = true,
             .free_on_error = true,
         });
+    }
+
+    pub fn write(self: *@This(), path: []const u8) !void {
+        var file = try std.fs.cwd().createFile(path, .{ .truncate = true });
+        defer file.close();
+        try std.zon.stringify.serialize(self, .{}, file.writer());
+    }
+
+    pub fn deinit(self: @This(), allocator: std.mem.Allocator) void {
+        std.zon.parse.free(allocator, self);
+    }
+};
+
+fn Json(T: type) type {
+    return struct {
+        arena: *std.heap.ArenaAllocator,
+        data: []const u8,
+        value: T,
+
+        pub fn init(slice: []const u8, value: std.json.Parsed(T)) @This() {
+            return .{
+                .arena = value.arena,
+                .value = value.value,
+                .data = slice,
+            };
+        }
+
+        pub fn deinit(self: @This()) void {
+            const allocator = self.arena.child_allocator;
+            self.arena.deinit();
+            allocator.free(self.data);
+            allocator.destroy(self.arena);
+        }
     };
+}
 
-    var client = std.http.Client{ .allocator = allocator };
-    defer client.deinit();
-
+fn fetch(allocator: std.mem.Allocator, client: *std.http.Client, uri: []const u8, R: type) !Json(R) {
     var header_buf: [16 * 1024]u8 = undefined;
 
-    var url = try std.Uri.parse("https://api.github.com/repos/Tired-Fox/winrt-json/tags");
+    const url = try std.Uri.parse(uri);
 
+    var req = try client.open(.GET, url, .{
+        .server_header_buffer = &header_buf,
+        .headers = .{
+            .content_type = .{
+                .override = "application/json"
+            }
+        }
+    });
+    defer req.deinit();
+
+    try req.send();
+    try req.wait();
+
+    const result_body = try req.reader().readAllAlloc(allocator, std.math.maxInt(usize));
+
+    // This is where its failing:
+    const result = try std.json.parseFromSlice(R, allocator, result_body, .{ .ignore_unknown_fields = true });
+    return Json(R).init(result_body, result);
+}
+
+fn download(client: *std.http.Client, uri: []const u8, out: []const u8) !void {
+    var header_buf: [8 * 1024]u8 = undefined;
+
+    const url = try std.Uri.parse(uri);
     var req = try client.open(.GET, url, .{ .server_header_buffer = &header_buf });
     defer req.deinit();
 
     try req.send();
     try req.wait();
 
-    const tag_rb = try req.reader().readAllAlloc(allocator, std.math.maxInt(usize));
-    defer allocator.free(tag_rb);
+    var file = try std.fs.cwd().createFile(out, .{ .truncate = true });
+    defer file.close();
 
-    // This is where its failing:
-    const tags = try std.json.parseFromSlice([]const Tag, allocator, tag_rb, .{ .ignore_unknown_fields = true });
+    var buf: [2048]u8 = undefined;
+    while (true) {
+        const n = try req.read(&buf);
+        if (n == 0) break;
+        try file.writeAll(buf[0..n]);
+    }
+}
+
+const UnzipOptions = struct {
+    clean: bool = false,
+    out: ?[]const u8 = null,
+};
+
+fn unzip(source: []const u8, options: UnzipOptions) !void {
+    var outdir = std.fs.cwd();
+    if (options.out) |out| {
+        if (options.clean) {
+            outdir.deleteTree(out) catch {};
+            try outdir.makePath(out);
+        }
+        outdir = try outdir.openDir(out, .{ .iterate = true });
+    }
+
+    const zip_file = try std.fs.cwd().openFile(source, .{});
+    defer zip_file.close();
+
+    const zip_stream = zip_file.seekableStream();
+    try std.zip.extract(outdir, zip_stream, .{});
+}
+
+fn downloadMetadata(allocator: std.mem.Allocator) !void {
+    var config = try Config.parse(allocator, "meta.zig.zon");
+    defer config.deinit(allocator);
+
+    var client = std.http.Client{ .allocator = allocator };
+    defer client.deinit();
+
+    const tags = try fetch(allocator, &client, "https://api.github.com/repos/Tired-Fox/winrt-json/tags", []const Tag);
     defer tags.deinit();
 
     if (tags.value.len == 0) {
         std.debug.print("\x1b[31m🗙\x1b[39m No winrt-json release found", .{});
-        std.process.exit(1);
+        return error.NoTags;
     }
 
     const latest = tags.value[0];
@@ -108,17 +194,12 @@ pub fn main() !void {
     );
     defer allocator.free(uri);
 
-    url = try std.Uri.parse(uri);
-    req.deinit();
-    req = try client.open(.GET, url, .{ .server_header_buffer = &header_buf });
-
-    try req.send();
-    try req.wait();
-
-    const release_rb = try req.reader().readAllAlloc(allocator, std.math.maxInt(usize));
-    defer allocator.free(release_rb);
-
-    const release = try std.json.parseFromSlice(Release, allocator, release_rb, .{ .ignore_unknown_fields = true });
+    const release = try fetch(
+        allocator,
+        &client,
+        uri,
+        Release
+    );
     defer release.deinit();
 
     for (release.value.assets) |asset| {
@@ -130,56 +211,17 @@ pub fn main() !void {
             }
             std.debug.print("[Release] {s}\n", .{ release.value.name });
 
-            {
-                url = try std.Uri.parse(asset.browser_download_url);
-                req.deinit();
-                req = try client.open(.GET, url, .{ .server_header_buffer = &header_buf });
+            try download(&client, asset.browser_download_url, "Metadata.zip");
+            config.current = latest.name;
+            config.digest = asset.digest;
+            try config.write("meta.zig.zon");
+            std.debug.print("  \x1b[36m⤓\x1b[39m Downloaded winrt-zig {s} to Metadata.zip\n", .{asset.name});
 
-                try req.send();
-                try req.wait();
+            try unzip("Metadata.zip", .{ .out = "metadata", .clean = true });
+            std.debug.print("  \x1b[33m↦\x1b[39m Unziped Metadata.zip to metadata/\n", .{});
 
-                {
-                    var file = try std.fs.cwd().createFile("Metadata.zip", .{ .truncate = true });
-                    defer file.close();
-
-                    var buf: [4096]u8 = undefined;
-                    while (true) {
-                        const n = try req.read(&buf);
-                        if (n == 0) break;
-                        try file.writeAll(buf[0..n]);
-                    }
-                }
-
-                {
-                    config.current = latest.name;
-                    config.digest = asset.digest;
-                    var file = try std.fs.cwd().createFile("config.zon", .{ .truncate = true });
-                    defer file.close();
-                    try std.zon.stringify.serialize(config, .{}, file.writer());
-                }
-
-                std.debug.print("  \x1b[36m⤓\x1b[39m Downloaded winrt-zig {s} to Metadata.zip\n", .{asset.name});
-            }
-
-            {
-                std.fs.cwd().deleteTree("metadata") catch {};
-                try std.fs.cwd().makePath("metadata");
-
-                const dir = try std.fs.cwd().openDir("metadata", .{ .iterate = true });
-
-                const zip_path: []const u8 = "Metadata.zip"; // Replace with your ZIP file path
-                const zip_file = try std.fs.cwd().openFile(zip_path, .{});
-                defer zip_file.close();
-
-                const zip_stream = zip_file.seekableStream();
-                try std.zip.extract(dir, zip_stream, .{});
-                std.debug.print("  \x1b[33m↦\x1b[39m Unziped Metadata.zip to metadata/\n", .{});
-            }
-
-            {
-                try std.fs.cwd().deleteTree("Metadata.zip");
-                std.debug.print("  \x1b[31m⌦\x1b[39m Deleted Metadata.zip\n", .{});
-            }
+            try std.fs.cwd().deleteTree("Metadata.zip");
+            std.debug.print("  \x1b[31m⌦\x1b[39m Deleted Metadata.zip\n", .{});
 
             std.debug.print("🎉 Metadata updated\n", .{});
 
@@ -187,6 +229,20 @@ pub fn main() !void {
         }
     } else {
         std.debug.print("\x1b[31m🗙\x1b[39m No release archive found for {s}\n", .{latest.name});
-        std.process.exit(1);
+        return error.NoArchive;
     }
+}
+
+pub fn main() !void {
+    var gpa = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer gpa.deinit();
+    const allocator = gpa.allocator();
+
+    try downloadMetadata(allocator);
+    // TODO: Iterate the namespace json files
+    //     TODO: Generate file structure that represents namespace
+    //     TODO: Iterate typedefs in namespace
+    //     TODO: Generate structs based on typedef
+    //     TODO: Reference <core> api for reusabel helpers
+    //     TODO: Collect needed imports and append to bottom
 }
